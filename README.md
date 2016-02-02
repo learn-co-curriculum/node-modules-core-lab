e31541d0cf0c80869a5296969ff0f6a923eda86b
Azat Mardan <hi@azat.co>
2016-02-02 09:34:01 -0800

    add changes :lock_with_ink_pen:

--- README.md
+++ README.md
@@ -16,6 +16,7 @@ In this lab, you'll start mastering the three core modules: fs, url and path.
 2. Read a data file `data/urls.txt` using `fs` and `path.join()` to point to the file
 3. Parse each line of the data from the `urls.txt` file with the `url` core module to get only the path name part, i.e., strip away the domain and protocol
 4. Write only the path names to a new file `output/paths.txt` using `path` for the path to the file.
+5. Print "Transformation completed" when the job is done.
 
 
 ### Extra Info
--- test/index.js
+++ test/index.js
@@ -2,9 +2,8 @@ var test = require('tape'),
   cp = require('child_process')
 
 test('node version', function (t) {
-  t.plan(3)
-  t.equal(process.versions.node, '5.1.0')
-  child = cp.exec('npm -v',
+  t.plan(6)
+  child = cp.exec('node url-transformer',
   function (error, stdout, stderr) {
     console.log('stdout: ' + stdout)
     console.log('stderr: ' + stderr)
@@ -13,6 +12,29 @@ test('node version', function (t) {
       console.log('exec error: ' + error)
     }
     stdout = stdout.replace('\n','')
-    t.equal(stdout, '3.3.12')
+    t.equal(stdout, 'Transformation completed')
+
+    var fs = require('fs')
+    var path = require('path')
+    var url = require('url')
+
+    var inputFile = path.join(__dirname, '..', 'data', 'urls.txt')
+    fs.readFile(inputFile, {encoding: 'utf8'}, function(error, urlsList){
+      var pathnameListExpected = urlsList.split('\n').map(function(fullUrl){
+        return url.parse(fullUrl).pathname
+      })
+      var outputFile = path.join(__dirname, '..','output', 'paths.txt')
+      fs.readFile(outputFile, {encoding: 'utf8'}, function(error, pathnameList) {
+        var pathnameListActual = pathnameList.split('\n')
+        t.equal(pathnameListExpected.length, pathnameListActual.length)
+        var l = pathnameListExpected.length
+        var n = Math.round(Math.random()*l)
+        t.equal(pathnameListExpected[n], pathnameListActual[n])
+        n = Math.round(Math.random()*l)
+        t.equal(pathnameListExpected[n], pathnameListActual[n])
+        n = Math.round(Math.random()*l)
+        t.equal(pathnameListExpected[n], pathnameListActual[n])
+      })
+    })
   })
 })
